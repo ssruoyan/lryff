c2f93c5e8f8021ea3ce6e06bd11feebabdbe8026
ssruoyan <ssruoyan@gmail.com>
2022-07-06 19:57:03 +0800

    feat: 初始化gatsby项目

---
+++ gatsby-config.ts
@@ -0,0 +1,45 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `李若炎的空间站`,
+    siteUrl: `https://www.lryff.com`,
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/favicon.png",
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    }
+  ],
+};
+
+export default config;
---
+++ src/pages/object-key-result-gift.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const ObjectKeyResultGift = () => {
+  return <p>OKR & Gift</p>
+}
+
+export default ObjectKeyResultGift
